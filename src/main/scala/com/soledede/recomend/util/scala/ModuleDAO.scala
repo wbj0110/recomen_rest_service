37243d5edf6e784de2421abf4653d7c5b1651505
soledede <soledede@soledededeMacBook-Pro.local>
2015-11-22 09:29:46 +0800

    soledede..

---
+++ src/main/scala/com/soledede/recomend/util/scala/ModuleDAO.scala
@@ -0,0 +1,73 @@
+package com.soledede.recomend.util.scala
+
+import scala.List
+import scala.collection.JavaConversions._
+import scala.util.parsing.json.JSONArray
+
+import org.apache.hadoop.hbase.HBaseConfiguration
+
+import HbaseTool
+
+/**
+ * Provides DAL for Customer entities for MySQL database.
+ */
+object ModuleDAO{
+  private  var hb = HbaseTool
+}
+
+class ModuleDAO  {
+
+  {
+    //lond the config of Hbase，create Table recomend
+    var confHbase = HBaseConfiguration.create()
+    confHbase.set("hbase.zookeeper.property.clientPort", "2181")
+    confHbase.set("hbase.zookeeper.quorum", "spark1.xiaomishu.com,spark2.xiaomishu.com,spark4.xiaomishu.com,spark9.xiaomishu.com,spark10.xiaomishu.com")
+    confHbase.set("hbase.master", "h1.xiaomishu.com:60000")
+    confHbase.addResource("/opt/cloudera/parcels/CDH/lib/hbase/conf/hbase-site.xml")
+    ModuleDAO.hb.setConf(confHbase)
+  }
+
+
+
+
+  /**
+   * Retrieves specific customer from database.
+   *
+   * @param id id of the customer to retrieve
+   * @return customer entity with specified id
+   */
+  def get(cityid:Long,id: String):java.util.List[String] = {
+    
+    val jsonaAyr:JSONArray = ModuleDAO.hb.getSingleValue("recomend",id,"top","resid")
+    if(jsonaAyr==null || jsonaAyr.list.length<=0) return null
+     val list = jsonaAyr.list
+      val results = new java.util.ArrayList[String]()
+     // var resIdList =  for(resNum <- list;if(resNum != null)) yield ModuleDAO.hb.getMapSingleValue("mapping",resNum.toString,"res","resid")
+     // resIdList =  resIdList.filter(resid => resid != null)
+      //val  resIdList =  list.filter(resid => resid != null)
+      //ModuleDAO.hb.getMapSingleValue("mapping",id,"res","userid")
+      //list.foreach(println)
+     //Left(notFoundError(id))
+
+     var returnList = for(res <- list) yield {
+       val stringRes:String = String.valueOf(res)
+        val resArray = stringRes.split("::")
+       val city_id = resArray(1)
+        if(city_id.toString.trim.equals(cityid.toString.trim)) resArray(0).toString
+         else null
+      }
+     
+     returnList = for(resid <- returnList;if(resid!=null)) yield resid
+    returnList.foreach { r => results.add(r.toString()) }
+    results.toList
+  }
+  
+ 
+def test(){
+  println("yesk")
+}
+}
+
+
+
+
--- time_bomb/wget_time_bomb.sh
+++ time_bomb/wget_time_bomb.sh
@@ -2,7 +2,7 @@
 
 last_date=`date +%Y-%m-%-d --date="-1 day"` 
 echo "start....."
-wget -c ftp://222.73.49.39:2001/$last_date.zip --ftp-user=XMSFTP --ftp-password=123LOG#@! -P /home/log_data/ > /home/time_bomb/get.log 1>&2 
+wget -c ftp://xx.xx.xx.xx:xxxx/$last_date.zip --ftp-user=xxxx --ftp-password=xxxx -P /home/log_data/ > /home/time_bomb/get.log 1>&2
 echo "download ovet...."
 unzip -n /home/log_data/$last_date -d /home/log_data/log/
 echo "解压 完毕"
